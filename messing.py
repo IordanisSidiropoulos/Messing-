cf031481d91c4faa00f94a62faf26b4e44243979
iordAnis Sidiropoulos <L00162407@student.lyit.ie>
2021-10-17 16:22:13 +0100

    MEssing Updated

--- messing.py
+++ messing.py
@@ -1,7 +1,10 @@
 print("Messing!!!")
 print("MESSING AGAIN")
 print("time is 7 o clock")
+<<<<<<< HEAD
 print("changed it online via Git Hub - PULL in computer")
 
 
 MISTAKE MISTAKE MISTAKE
+=======
+>>>>>>> parent of 79f5863... PULLING message
